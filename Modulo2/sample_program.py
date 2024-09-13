6be9afa907656384cd7ba260f02aca60ec9c8eff
giurd <salvatore.giurdanella@sintegra.it>
2024-09-14 01:20:24 +0200

    sample program module 1

---
+++ Modulo2/sample_program.py
@@ -0,0 +1,9 @@
+# Single line comment
+
+""" 
+    This is a multi line 
+    comment
+"""
+
+for x in range(3):
+    print ("Hello World at "+ str(x+1) + " times")
--- test.py
+++
@@ -1,2 +0,0 @@
-for x in range(2):
-    print ("Hello World at "+ str(x+1) + " times")
